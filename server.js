1f57b81bc210ac7179b088981b8ab5ee82a2ebaa
clandau <so.super@gmail.com>
2018-04-20 20:42:46 -0400

    add file upload capabilities with multer

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
+//multer middleware to handle the file upload
+const multer = require('multer');
+const fileUpload = multer({ dest: 'fileUploads/' });
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -11,6 +15,12 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
+
+app.post('/api/fileanalyze', fileUpload.single('upfile'), (req, res, next) => {
+    //get info from file
+    console.log(req.file.size);
+    res.send({ 'file size in bytes' : req.file.size })
+})
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
